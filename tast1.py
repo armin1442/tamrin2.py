db7c9bae48b4d65cf7871f871776b69bafe36403
arminjoya <94761705+armin1442@users.noreply.github.com>
2022-01-30 21:10:32 +0330

    Add files via upload

---
+++ tast1.py
@@ -0,0 +1,49 @@
+x = 4
+y = "hello"
+print(x)
+print(y)
+
+x = 4 
+x= "Sally" 
+print(x)
+
+a = str(5)
+b = int(6)
+c = float(7)
+
+a = (8)
+b = "armin"
+print(type(a))
+print(type(b))
+
+a , b , c = "eeg" , "green" , "car" 
+print(a)
+print(c)
+print(b) 
+
+p = o = d = "ball"
+print(p)
+print(o)
+print(d)
+
+folder = "lin","apple","food"
+s, v, n = folder
+print(s)
+print(v)
+print(n)
+
+x = "armin"
+print("Python is " + x)
+
+v = 9
+g = 10
+print(v + g)
+
+b = "python is"
+n = "awesome"
+z = b + n
+print(z)
+
+n = 5
+m = "Armin"
+print(n , m)
---
+++ tast2.py
@@ -0,0 +1,44 @@
+a = "hello , world!"
+print(a[3:30])
+
+a = "hello , world!"
+print(a[:5])
+
+a = "hello , world!"
+print(a[:2])
+
+a = "hello , world!"
+print(a[-5 : -2])
+
+b = "Hello, World!"
+print(b.upper())
+
+b = "Hello, World!"
+print(b.lower())
+
+b = "Hello, World!"
+print(b.strip())
+
+b = "Hello, World!"
+print(b.replace("H", "J"))
+
+b = "Hello, World!"
+print(b.split(","))
+
+n = "hello"
+f = "word"
+b = n + f
+print(b)
+
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+age = 36
+txt = "My name is John, I am " + age
+print(txt)
+
+
+
+
